15279775d14d7ec3aaf8cada5cbc5faa68e8c515
00raiser19980427 <a218071@ns.kogakuin.ac.jp>
2020-02-09 01:22:55 +0900

    経路の関数をそろえた

--- move/routeSet/routeShape/routeShape.h
+++ move/routeSet/routeShape/routeShape.h
@@ -11,9 +11,16 @@
 #include <math.h>
 
 class routeShape {
+
+private:
+	float nijo[3];
+	float bunsu;
 public:
 	float bezier3(float anc0, float anc1, float anc2, float anc3, float t);
 	float bezier5(float anc0, float anc1, float anc2, float anc3, float anc4,float anc5,float t);
+	float bezier_Circle(float anc0,float anc1,float anc2,float theta,float t);
+	float Straight_up(float anc0 , float anc1 , float t);
+	float Straight_down(float anc0 , float anc1 , float t);
 };
 
 #endif /* HIROKI_ROUTESHAPE_H_ */
--- move/routeSet/routeShape/routetShape.cpp
+++ move/routeSet/routeShape/routetShape.cpp
@@ -14,3 +14,19 @@ float routeShape::bezier3(float anc0,float anc1,float anc2,float anc3,float t){
 float routeShape::bezier5(float anc0, float anc1, float anc2, float anc3, float anc4,float anc5,float t){
 	return (1-t)*(1-t)*(1-t)*(1-t)*(1-t)*anc0+5*(1-t)*(1-t)*(1-t)*(1-t)*t*anc1+10*(1-t)*(1-t)*(1-t)*t*t*anc2+10*(1-t)*(1-t)*t*t*t*anc3+5*(1-t)*t*t*t*t*anc4+t*t*t*t*t*anc5;
 }
+
+float routeShape::bezier_Circle(float anc0,float anc1,float anc2,float theta,float t){
+	nijo[0]=(1-t)*(1-t);
+	nijo[1]=2*(1-t)*t*cos(theta/2);
+	nijo[3]=t*t;
+	bunsu = (nijo[0] + nijo[1] + nijo[2]);
+	return nijo[0]*anc0/bunsu + nijo[1]*anc1/bunsu + nijo[2]*anc2/bunsu;
+}
+
+float routeShape::Straight_up(float anc0 , float anc1 , float t){
+	return anc0 + (anc1 - anc0)*t*t;
+}
+
+float routeShape::Straight_down(float anc0 , float anc1 , float t){
+	return anc1 + (anc0 - anc1)*(t-1)*(t-1);
+}
