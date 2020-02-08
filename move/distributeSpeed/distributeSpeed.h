e90e7d62c79a891427a65dec65b7e6324c205457
00raiser19980427 <a218071@ns.kogakuin.ac.jp>
2020-02-09 05:15:02 +0900

    ライブラリの値をアドレスで受け取る

--- move/distributeSpeed/distributeSpeed.h
+++ move/distributeSpeed/distributeSpeed.h
@@ -16,11 +16,11 @@ private:
 	float PI = 3.1415926535;
 	float RR = 0.365;			//machine radius
 public:
-	float V1;
-	float V2;
-	float V3;
-	float V4;
-	void distribute(float x,float y,float yaw,float now_theta); //calculate necessary Volume for omniwheel
+	float *V1;
+	float *V2;
+	float *V3;
+	float *V4;
+	void distribute(float x,float y,float yaw,float *now_theta); //calculate necessary Volume for omniwheel
 };
 
 #endif /* HIROKI_DISTRIBUTESPEED_H_ */
