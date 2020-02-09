8bf10c8618dc1fa0553626a593d84ca848e844b5
00raiser19980427 <a218071@ns.kogakuin.ac.jp>
2020-02-09 17:06:43 +0900

    戻した

--- move/distributeSpeed/distributeSpeed.cpp
+++ move/distributeSpeed/distributeSpeed.cpp
@@ -7,9 +7,9 @@
 
 #include <HIROKI/move/distributeSpeed/distributeSpeed.h>
 
-void distributeSpeed::distribute(float x,float y,float yaw,float *now_theta){
-	*V1 = -x*sin(PI*1/4+*now_theta) + y*cos(PI*1/4+*now_theta) + RR*yaw;
-	*V2 =  x*cos(PI*1/4+*now_theta) + y*sin(PI*1/4+*now_theta) + RR*yaw;
-	*V3 =  x*sin(PI*1/4+*now_theta) - y*cos(PI*1/4+*now_theta) + RR*yaw;
-	*V4 = -x*cos(PI*1/4+*now_theta) - y*sin(PI*1/4+*now_theta) + RR*yaw;
+void distributeSpeed::distribute(float x,float y,float yaw,float now_theta){
+	V1 = -x*sin(PI*1/4+now_theta) + y*cos(PI*1/4+now_theta) + RR*yaw;
+	V2 =  x*cos(PI*1/4+now_theta) + y*sin(PI*1/4+now_theta) + RR*yaw;
+	V3 =  x*sin(PI*1/4+now_theta) - y*cos(PI*1/4+now_theta) + RR*yaw;
+	V4 = -x*cos(PI*1/4+now_theta) - y*sin(PI*1/4+now_theta) + RR*yaw;
 }
